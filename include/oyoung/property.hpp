b5d16df30a4b4e6bca1de402e403a22c99197c86
欧阳建华 <yangjianhua@iplusbot.com>
2019-02-15 11:13:52 +0800

    Add property.hpp(class oyoung::property<T>)

---
+++ include/oyoung/property.hpp
@@ -0,0 +1,135 @@
+//
+// Created by oyoung on 19-2-15.
+//
+
+#ifndef DISPATCH_PROPERTY_HPP
+#define DISPATCH_PROPERTY_HPP
+
+#include <memory>
+#include <functional>
+
+namespace oyoung {
+
+    template <typename T>
+    struct property {
+        using value_type = typename std::remove_reference<T>::type;
+        using const_value_type = const value_type;
+        using reference_type = value_type &;
+        using const_referennce_type =  const value_type &;
+
+        using get_function = std::function<reference_type()>;
+        using set_function = std::function<void(const_referennce_type)>;
+        using will_set_function = std::function<bool(const_referennce_type)>;
+        using did_set_function = std::function<void(const_referennce_type, const_referennce_type)>;
+
+        property(const_referennce_type val)
+                : _value(std::make_shared<value_type>(val)) {
+            _get = [=] () -> reference_type { return ref() ;};
+            _set = [=] (const_referennce_type other) {
+                *_value = other;
+            };
+        }
+
+        value_type value() const {
+            return _value ? *_value : _get();
+        }
+        reference_type ref() {
+            return _value ? *_value : _get();
+        }
+        const_referennce_type ref() const {
+            return _value ? *_value : _get();
+        }
+
+
+        property() {}
+
+        void bind(reference_type reference,
+                  const will_set_function& check = nullptr,
+                  const did_set_function& changed = nullptr) {
+            _get = [&] () -> reference_type { return reference ;};
+            _will_set = check;
+            _set = [&] (const_referennce_type other) {
+                reference = other;
+            };
+            _did_set = changed;
+        }
+
+        void assign(const_referennce_type val) {
+
+            if(_will_set) {
+                /// check new value is valid
+                if(!_will_set(val)) return;
+            }
+
+            auto old = _get();
+            auto changed = old != val;
+
+
+            /// set new value
+            if(_set) {
+                _set(val);
+            }
+
+            /// value changed
+            if(changed && _did_set) {
+                _did_set(val, old);
+            }
+        }
+
+        void swap(property<T>& other) {
+            _value.swap(other._value);
+            _get.swap(other._get);
+            _will_set.swap(other._will_set);
+            _set.swap(other._set);
+            _did_set.swap(other._did_set);
+        }
+
+        void swap(reference_type reference) {
+            std::swap(reference, ref());
+        }
+
+        property&operator=(const_referennce_type val) {
+            assign(val);
+            return *this;
+        }
+
+        operator value_type() const {
+            return _get();
+        }
+
+
+        void willSet(const will_set_function& function) {
+            _will_set = function;
+        }
+
+        void didSet(const did_set_function& function) {
+            _did_set = function;
+        }
+
+    private:
+        get_function _get;
+        will_set_function _will_set;
+        set_function _set;
+        did_set_function _did_set;
+
+    private:
+        std::shared_ptr<value_type> _value;
+    };
+
+    template <typename IStream, typename T>
+    IStream& operator>>(IStream& in, property<T>& prop) {
+        auto value = typename property<T>::value_type {};
+        in >> value;
+        prop = value;
+        return in;
+    };
+
+    template <typename OStream, typename T>
+    OStream& operator<<(OStream& out, const property<T>& prop) {
+        out << prop.ref();
+        return out;
+    };
+
+}
+
+#endif //DISPATCH_PROPERTY_HPP
---
+++ property_main.cc
@@ -0,0 +1,54 @@
+//
+// Created by oyoung on 19-2-15.
+//
+
+#include <oyoung/property.hpp>
+#include <iostream>
+
+
+class Person {
+public:
+    oyoung::property<int> age{1};
+    oyoung::property<std::string> gender{"Female"};
+    oyoung::property<std::string> name {};
+    Person() {
+        age.willSet([=] (int newVal) {
+            return newVal > age;
+        });
+        age.didSet([=](int val, int old) {
+            std::cout << "Age changed: ( current: " << val << ", old: " << old << ")" << std::endl;
+        });
+        name.bind(_name);
+    }
+
+    std::string& internal_name() { return _name; }
+
+private:
+    std::string _name {"Noname"};
+};
+
+int main(int, char**)
+{
+
+    Person person;
+
+    std::cout << person.internal_name() << ", gender: " << person.gender << ", age: " << person.age << std::endl;
+
+
+    /// assign property stored value
+    person.age = 26;
+    person.gender = "Male";
+
+
+    /// assign bind member _name;
+    person.name = "oyoung";
+
+    std::cout << person.internal_name() << ", gender: " << person.gender << ", age: " << person.age << std::endl;
+
+    /// assign age will be reject, because 22 less than the old age 26
+    person.age = 22;
+    std::cout << person.internal_name() << ", gender: " << person.gender << ", age: " << person.age << std::endl;
+
+
+    return 0;
+}
