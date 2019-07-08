b21d6673a90232c17dbdfd01dde7fb324f676491
欧阳建华 <yangjianhua@iplusbot.com>
2019-07-08 17:00:28 +0800

    add defer support

---
+++ defer_main.cc
@@ -0,0 +1,56 @@
+//
+// Created by oyoung on 19-7-8.
+//
+
+#include <oyoung/defer.hpp>
+#include <iostream>
+
+struct Test {
+
+    explicit Test(int n): m_number(new int(n)) {
+        std::cout << "create number: " << n << std::endl;
+        defer [=] {
+            if(m_number) {
+                std::cout << "release number: " << n << std::endl;
+                delete  m_number;
+                m_number = nullptr;
+            }
+        };
+    }
+
+    void clear() {
+
+        std::cout << "clear number: " <<  (m_number ? std::to_string(*m_number): "null") << std::endl;
+
+        delete  m_number;
+        m_number = nullptr;
+    }
+
+private:
+    USE_DEFER;
+    int *m_number{nullptr};
+};
+
+int  main(int, char**) {
+
+    USE_DEFER;
+
+    std::cout << "create one" << std::endl;
+
+    defer [=] {
+        std::cout << "destroy one" << std::endl;
+    };
+
+    std::cout << "create two" << std::endl;
+
+    defer [=] {
+        std::cout << "destroy two" << std::endl;
+    };
+
+    Test test1(1);
+
+    test1.clear();
+
+    Test test2(2);
+
+}
---
+++ include/oyoung/defer.hpp
@@ -0,0 +1,34 @@
+//
+// Created by oyoung on 19-7-8.
+//
+
+#ifndef DISPATCH_DEFER_HPP
+#define DISPATCH_DEFER_HPP
+
+#include <vector>
+#include <functional>
+
+namespace oyoung {
+    struct Defer {
+        Defer& operator+=(std::function<void()> block) {
+            m_defers.emplace_back(std::move(block));
+            return *this;
+        }
+
+        ~Defer() {
+            while (!m_defers.empty()) {
+                if(m_defers.back()) m_defers.back()();
+                m_defers.pop_back();
+            }
+        }
+    private:
+        std::vector<std::function<void()>> m_defers;
+    };
+
+
+}
+
+#define USE_DEFER oyoung::Defer __defer__
+#define defer __defer__ +=
+
+#endif //DISPATCH_DEFER_HPP
